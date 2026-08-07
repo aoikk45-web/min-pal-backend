31cea75afcc2a01a750077a9905c97d0d1664205
kaori <aoikk45@gmail.com>
2026-08-08 07:34:27 +0900

    fix import paths

--- app/routers/share.py
+++ app/routers/share.py
@@ -10,8 +10,8 @@ from app.models.share_message import (
     ShareMessageGroup,
     PlayerShareMessage,
 )
-from backend.app.services.score_service import calculate_scores
-from backend.app.services.share_service import (
+from app.services.score_service import calculate_scores
+from app.services.share_service import (
     generate_detail_message,
     generate_simple_message,
     generate_emoji_message,
